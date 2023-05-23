a0445adc8302ab1939df2208b7f5b22ae635b200
muttalipSahin <muttalip9801@gmail.com>
2023-05-23 13:29:50 +0200

    rest standard enpoints almost finished

--- src/main/java/com/gni/banking/Model/TransactionRequestDTO.java
+++ src/main/java/com/gni/banking/Model/TransactionRequestDTO.java
@@ -1,15 +1,13 @@
 package com.gni.banking.Model;
 
-import java.util.Date;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
 public class TransactionRequestDTO {
-
-    private String ibanFrom;
-
-    private String ibanTo;
-
+    private long id;
+    private String accountFrom;
+    private String accountTo;
     private double amount;
-
-    private String description;
-
 }
