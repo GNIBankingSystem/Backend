99a1070f1e41aed0f02eb472b7e18afc2c29fcfe
CaptainJeppie <89974982+CaptainJeppie@users.noreply.github.com>
2023-06-02 14:33:47 +0200

    small update

--- src/main/java/com/gni/banking/Configuration/DataSeeder.java
+++ src/main/java/com/gni/banking/Configuration/DataSeeder.java
@@ -21,18 +21,18 @@ public class DataSeeder implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         Account account = new Account();
-        account.setIban("RO123456789");
+        account.setIban("NL01INHO0000000001");
         account.setUserId(1);
         account.setType(AccountType.Current);
         account.setAbsoluteLimit(1000);
         account.setCurrency(Currency.EUR);
-        account.setBalance(550);
+        account.setBalance(550000000);
         account.setStatus(Status.Open);
         accountService.add(account);
 
         Account account1 = new Account();
-        account1.setIban("RO987654321");
-        account1.setUserId(1);
+        account1.setIban("NL01INHO456874318");
+        account1.setUserId(2);
         account1.setType(AccountType.Current);
         account1.setAbsoluteLimit(1000);
         account1.setCurrency(Currency.EUR);
