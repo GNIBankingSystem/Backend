0d37907e713eeaa9230c3397717e9b58fc7cd63a
muttalipSahin <66790473+muttalipSahin@users.noreply.github.com>
2023-06-03 16:21:09 +0200

    Transaction crud +checks on transaction finsihed
    The checks on editing and adding a transaction is done
    Checks on account type status +account type

--- src/main/java/com/gni/banking/Configuration/DataSeeder.java
+++ src/main/java/com/gni/banking/Configuration/DataSeeder.java
@@ -5,18 +5,23 @@ import com.gni.banking.Enums.Currency;
 import com.gni.banking.Enums.Status;
 import com.gni.banking.Model.Account;
 import com.gni.banking.Model.AccountRequestDTO;
+import com.gni.banking.Model.Transaction;
 import com.gni.banking.Service.AccountService;
+import com.gni.banking.Service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Component
 public class DataSeeder implements ApplicationRunner {
 
     @Autowired
     AccountService accountService;
-
+    @Autowired
+    TransactionService transactionService;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
@@ -39,5 +44,26 @@ public class DataSeeder implements ApplicationRunner {
         account1.setBalance(55);
         account1.setStatus(Status.Open);
         accountService.add(account1);
+//
+//        Transaction transaction1 = new Transaction();
+//        transaction1.setId(1);
+//        transaction1.setTimeStamp(new Date());
+//        transaction1.setAccountFrom("NL01INHO0000000001");
+//        transaction1.setAccountTo("NL01INHO456874318");
+//        transaction1.setAmount(50);
+//        transaction1.setPerformedBy(1);
+//        transaction1.setArchived(false);
+//        transactionService.add(transaction1);
+//
+//        Transaction transaction2 = new Transaction();
+//        transaction2.setId(2);
+//        transaction2.setTimeStamp(new Date());
+//        transaction2.setAccountFrom("NL01INHO456874318");
+//        transaction2.setAccountTo("NL01INHO0000000001");
+//        transaction2.setAmount(100);
+//        transaction2.setPerformedBy(2);
+//        transaction2.setArchived(false);
+//        transactionService.add(transaction2);
+
     }
 }
--- src/main/java/com/gni/banking/Service/TransactionService.java
+++ src/main/java/com/gni/banking/Service/TransactionService.java
@@ -1,17 +1,23 @@
 package com.gni.banking.Service;
 
+import com.gni.banking.Enums.AccountType;
+import com.gni.banking.Enums.Status;
+import com.gni.banking.Model.Account;
 import com.gni.banking.Model.Transaction;
-import com.gni.banking.Model.TransactionResponseDTO;
 import com.gni.banking.Repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
+import java.util.regex.Pattern;
 
 @Service
 public class TransactionService {
     @Autowired
     private TransactionRepository repository;
+    @Autowired
+    private AccountService accountService;
 
     public List<Transaction> getAll() {
         return (List<Transaction>) repository.findAll();
@@ -21,16 +27,99 @@ public class TransactionService {
         return repository.findById(id).orElse(null);
     }
 
-    public Transaction add(Transaction transaction) {
+    public Transaction add(Transaction transaction) throws Exception {
+        //checks on account type and amount
+        checksOnMakingAndEditingTransaction(transaction);
+        transaction.setTimeStamp(new Date());
+        //update balance on accounts
+        updateBalanceOfAccounts(accountService.getByIban(transaction.getAccountFrom()), accountService.getByIban(transaction.getAccountTo()), transaction.getAmount());
         return repository.save(transaction);
     }
 
-    public Transaction update(Transaction transaction, long id) {
+    private void checksOnMakingAndEditingTransaction(Transaction transaction) throws Exception {
+        checkIfIbansExists(transaction.getAccountFrom(), transaction.getAccountTo());
+        checkAccountStatus(accountService.getByIban(transaction.getAccountFrom()), accountService.getByIban(transaction.getAccountTo()));
+        checkAccountTypes(transaction.getAccountFrom(), transaction.getAccountTo());
+        checkAmountsOnTransaction(transaction.getAccountFrom(), transaction.getAccountTo(), transaction.getAmount());
+    }
+
+    public Transaction update(Transaction transaction, long id) throws Exception {
+        checksOnMakingAndEditingTransaction(transaction);
         return repository.save(transaction);
     }
 
     public void delete(long id) {
         repository.archiveById(id);
     }
+
+
+    private boolean checkAccountTypes(String ibanFrom, String ibanTo) throws Exception {
+        Account accountFrom = accountService.getByIban(ibanFrom);
+        Account accountTo = accountService.getByIban(ibanTo);
+        if (accountFrom.getType() == AccountType.Savings && accountTo.getType() == AccountType.Savings)
+            throw new Exception("You can't transfer money between two savings accounts");
+        if (accountFrom.getType() == AccountType.Savings && accountTo.getType() == AccountType.Current) {
+            if (accountFrom.getUserId() == accountTo.getUserId()) {
+                return true;
+            }
+            throw new Exception("You can't transfer money from savings account to another user's current account");
+        }
+        if (accountFrom.getType() == AccountType.Current && accountTo.getType() == AccountType.Savings) {
+            if (accountFrom.getUserId() == accountTo.getUserId()) {
+                return true;
+            }
+            throw new Exception("You can't transfer money from current account to another user's savings account");
+        }
+        return true;
+    }
+
+    private boolean checkAmountsOnTransaction(String ibanFrom, String ibanTo, double amount) throws Exception {
+        Account accountFrom = accountService.getByIban(ibanFrom);
+        Account accountTo = accountService.getByIban(ibanTo);
+        if (accountFrom.getBalance() < amount)
+            throw new Exception("Not enough money on account");
+        if(accountFrom.getAbsoluteLimit() < amount)
+            throw new Exception("Absolute limit exceeded");
+        if (accountFrom.getCurrency() != accountTo.getCurrency())
+            throw new Exception("You can't transfer money between accounts with different currencies");
+        return true;
+    }
+
+    private void updateBalanceOfAccounts(Account accountFrom, Account accountTo, double amount) throws Exception {
+        accountFrom.setBalance(accountFrom.getBalance() - amount);
+        accountTo.setBalance(accountTo.getBalance() + amount);
+        accountService.update(accountFrom, accountFrom.getIban());
+        accountService.update(accountTo, accountTo.getIban());
+
+    }
+
+    private boolean checkAccountStatus(Account accountFrom, Account accountTo) throws Exception {
+        if (accountFrom.getStatus().equals(Status.Closed) || accountTo.getStatus().equals(Status.Closed))
+            throw new Exception("Cannot perform transaction on closed account");
+        return true;
+    }
+
+    private void checkIfIbansExists(String accountFrom, String accountTo) {
+        //check if iban is in valid format
+        if (!isValidIbanFormat(accountFrom) || !isValidIbanFormat(accountTo))
+            throw new IllegalArgumentException("Invalid IBAN");
+
+        //check if iban exists
+        if (accountService.getByIban(accountFrom) == null || accountService.getByIban(accountTo) == null)
+            throw new IllegalArgumentException("IBAN does not exist");
+    }
+
+
+    public boolean isValidIbanFormat(String iban) {
+        // Remove all the spaces from the iban string
+        iban = iban.replace(" ", "");
+
+        // Define the pattern for the IBAN
+        String regex = "^[A-Z]{2}\\d{2}[A-Z0-9]{1,30}$";
+
+        // Check if the IBAN matches the pattern
+        return Pattern.matches(regex, iban);
+    }
+
 }
 
