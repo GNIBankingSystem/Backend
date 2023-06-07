b3a01d375fed1a4d6586967d3da8898f92995339
CaptainJeppie <89974982+CaptainJeppie@users.noreply.github.com>
2023-06-07 10:54:36 +0200

    cross

--- src/main/java/com/gni/banking/Controller/TransactionController.java
+++ src/main/java/com/gni/banking/Controller/TransactionController.java
@@ -18,6 +18,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/transactions")
 
 public class TransactionController {
