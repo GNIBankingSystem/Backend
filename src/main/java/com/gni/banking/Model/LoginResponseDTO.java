2e10ac21d7ae5930573f583dbdd69821b69d7221
CaptainJeppie <89974982+CaptainJeppie@users.noreply.github.com>
2023-06-08 15:10:16 +0200

    backend login responds with id and role extra

--- src/main/java/com/gni/banking/Model/LoginResponseDTO.java
+++ src/main/java/com/gni/banking/Model/LoginResponseDTO.java
@@ -1,9 +1,12 @@
 package com.gni.banking.Model;
 
+import com.gni.banking.Enums.Role;
 import lombok.Data;
 
 @Data
 public class LoginResponseDTO {
+    private long id;
     private String username;
     private String token;
+    private Role role;
 }
