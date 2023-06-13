6d3e84142c1844ce9cd0d43324e70d8c4d236b1d
muttalipSahin <66790473+muttalipSahin@users.noreply.github.com>
2023-06-13 12:44:05 +0200

    update exception handler

--- src/main/java/com/gni/banking/Exceptions/Handler/CustomExceptionHandler.java
+++ src/main/java/com/gni/banking/Exceptions/Handler/CustomExceptionHandler.java
@@ -7,6 +7,9 @@ import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class CustomExceptionHandler {
 
@@ -14,21 +17,27 @@ public class CustomExceptionHandler {
             NotEnoughBalanceException.class,
             LimitOnTransactionExceededException.class,
             InvalidAccountCurrenyOnTransactionException.class})
-    public ResponseEntity<ErrorResponse> handleBadRequestExceptions(Exception e) {
-        ErrorResponse errorResponse = ErrorResponse.create(e, HttpStatus.BAD_REQUEST, e.getMessage());
+    public ResponseEntity<Map<String,String>> handleBadRequestExceptions(Exception e) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("errorMessage", e.getClass().getSimpleName());
+        errorResponse.put("message", e.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(InvalidAccountStatusException.class)
-    public ResponseEntity<ErrorResponse> handleForbiddenException(Exception e) {
-        ErrorResponse errorResponse = ErrorResponse.create(e, HttpStatus.FORBIDDEN, e.getMessage());
+    public ResponseEntity<Map<String,String>> handleForbiddenException(Exception e) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("errorMessage", e.getClass().getSimpleName());
+        errorResponse.put("message", e.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
     }
 
     // handle other exceptions
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleUnknownException(Exception e) {
-        ErrorResponse errorResponse = ErrorResponse.create(e, HttpStatus.BAD_REQUEST, e.getMessage());
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Map<String,String>> handleUnknownException(Exception e) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("errorMessage", e.getClass().getSimpleName());
+        errorResponse.put("message", e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
     }
 }
